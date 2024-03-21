9418741daf48f28ecb12b67df7cb6e8a249b72ee
EC2 Default User <ec2-user@ip-172-31-24-5.ap-northeast-1.compute.internal>
2024-03-21 15:31:40 +0900

    ブログ投稿作成画面

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\http\Controllers\PostController;
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +13,7 @@ use App\http\Controllers\PostController;
 |
 */
 
-Route::get('/posts',[PostController::class, 'index']);
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::post('/posts', [PostController::class, 'store']);
